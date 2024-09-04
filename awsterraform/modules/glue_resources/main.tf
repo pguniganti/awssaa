631ba1fd095be5dc875755c9a906acac57e10613
Priyank Guniganti <priyank.guniganti@wnins.com>
2024-09-03 21:25:52 -0400

    updated main.tf file

--- awsterraform/modules/glue_resources/main.tf
+++ awsterraform/modules/glue_resources/main.tf
@@ -33,6 +33,28 @@ resource "aws_iam_role" "glue_role" {
   })
 }
 
+resource "aws_iam_role_policy" "glue_s3_access" {
+  name = "GlueS3Access"
+  role = aws_iam_role.glue_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::priyank-test-v7",
+          "arn:aws:s3:::priyank-test-v7/*"
+        ]
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "glue_policy" {
   role       = aws_iam_role.glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
